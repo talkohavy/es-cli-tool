3abeb71ec1381dbe7d10123555a40762da4c5530
Tal Kohavy <talkohavy@gmail.com>
2024-12-05 19:38:25 +0200

    removed unused helper functions

--- src/commands/add/helpers/useVsCodeAsEditor.ts
+++ src/commands/add/helpers/useVsCodeAsEditor.ts
@@ -1,6 +1,6 @@
 import { EditorTypes } from '../../../constants/types.js';
 
-function useVsCodeAsEditor(editor: any) {
+export function useVsCodeAsEditor(editor: any) {
   editor.editor.bin = EditorTypes.Code;
   editor.editor.args = editor.editor.args.concat(
     '--wait', // <--- IMPORTANT!!! without this vscode will open and exit immediately!
@@ -8,5 +8,3 @@ function useVsCodeAsEditor(editor: any) {
     // Some notable flags that are available: '-n', 'tmp', '--verbose', '--log', 'debug', '--disable-extensions', '--in-process-gpu'
   );
 }
-
-export { useVsCodeAsEditor };
