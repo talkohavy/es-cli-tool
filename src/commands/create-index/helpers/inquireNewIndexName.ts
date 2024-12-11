881f26a9f887b263d30581185ca51ab4c3ac9ce6
Tal Kohavy <talkohavy@gmail.com>
2024-12-11 22:15:38 +0200

    docs(lvlup): added a line-break before inquireNewIndexName

--- src/commands/create-index/helpers/inquireNewIndexName.ts
+++ src/commands/create-index/helpers/inquireNewIndexName.ts
@@ -2,6 +2,8 @@ import { input } from '@inquirer/prompts';
 import { COLORS } from '../../../constants/colors.js';
 
 export async function inquireNewIndexName() {
+  console.log('');
+
   console.log(`${COLORS.green} ✨  Please enter a new index name:`);
 
   const newIndexName = await input({ message: 'Index name:' });
