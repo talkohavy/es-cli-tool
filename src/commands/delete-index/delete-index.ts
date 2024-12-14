5206102d3786e5331c0c4983cfdc6a283f149da0
Tal Kohavy <talkohavy@gmail.com>
2024-12-14 12:24:22 +0200

    docs(lvlup): added a confirmation check to the delete-index command. Added a new flag called "--sure" that the user can pass in order to pass the check.

--- src/commands/delete-index/delete-index.ts
+++ src/commands/delete-index/delete-index.ts
@@ -2,6 +2,7 @@ import { Argv } from 'yargs';
 import { COLORS } from '../../constants/colors.js';
 import { colorizeJson } from '../../utils/colorize-json/colorize-json.js';
 import { getAllIndexesNames } from '../../utils/getAllIndexesNames.js';
+import { inquireConfirm } from '../../utils/inquires/inquireConfirm.js';
 import { inquireSelectFromList } from '../../utils/inquires/inquireSelectFromList.js';
 import { logger } from '../../utils/logger/logger.js';
 import { executeDeleteIndexQuery } from './helpers/executeDeleteIndexQuery.js';
@@ -16,15 +17,23 @@ export const deleteIndexBuilder: any = (yargs: Argv) => {
       description: 'Specify the target index.',
     })
     .example('es-cli-tool delete-index --index users', 'Deletes the users index.');
+  yargs
+    .option('--sure', {
+      alias: 'y',
+      type: 'boolean',
+      description: 'auto-pass the "are you sure" inquire.',
+    })
+    .example('es-cli-tool delete-index --sure', 'Deletes the users index without user confirmation.');
 };
 
 type DeleteIndexProps = {
   index: string;
+  sure: boolean;
   color: boolean;
 };
 
 export async function deleteIndex(props: DeleteIndexProps) {
-  const { index, color: shouldColorize } = props;
+  const { index, sure: isSure, color: shouldColorize } = props;
 
   const indexNamesArr = await getAllIndexesNames();
 
@@ -44,6 +53,10 @@ export async function deleteIndex(props: DeleteIndexProps) {
 
     return;
   }
+
+  const shouldDelete = isSure ?? (await inquireConfirm());
+
+  if (!shouldDelete) return;
 
   const responseRaw = await executeDeleteIndexQuery(selectedIndex);
 
