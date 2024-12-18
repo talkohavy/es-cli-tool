c7c7a6f4881260001039376536c8ec3f9df50d28
Tal Kohavy <talkohavy@gmail.com>
2024-12-18 23:57:25 +0200

    Cleaned the index.ts file. No more comments.

--- src/index.ts
+++ src/index.ts
@@ -57,44 +57,11 @@ import { showVersion } from './common/utils/showVersion.js';
 
 const __no_op__: any = () => {};
 
-type ArgsV = {
-  $0: any;
-  _: Array<string>;
-} & Record<string, string | number | boolean>;
-
+// A full description on each of the following functions is found under the `lvlup` project.
 const yargsInstance = yargs(hideBin(process.argv))
   .completion()
-  /**
-   * @description
-   * Set the name of your script ($0). Default is the base filename executed by node (process.argv[1] or process.argv[0] for built electron apps)
-   *
-   * The name will appear at to the of the hemp menu
-   */
   .scriptName(`${COLORS.green}es-cli-tool${COLORS.stop}`)
-  /**
-   * @description
-   * I put version to `false`, because yargs do not support -v, only --version, and I would like to have both.
-   *
-   * Add an option (e.g. --version) that displays the version number (given by the version parameter) and exits the process. By default yargs enables version for the --version option.
-   *
-   * If no arguments are passed to version (.version()), yargs will parse the package.json of your module and use its version value.
-   *
-   * If the boolean argument false is provided, it will disable --version.
-   */
   .version(false)
-  /**
-   * @description
-   *
-   * Define the commands exposed by your application.
-   *
-   * cmd should be a string representing the command or an array of strings representing the command and its aliases. Read more about command aliases in the subsection below.
-   *
-   * Use desc to provide a description for each command your application accepts (the values stored in argv._). Set desc to false to create a hidden command. Hidden commands don't show up in the help output and aren't available for completion.
-   *
-   * Optionally, you can provide a builder object to give hints about the options that your command accepts:
-   *
-   * IMPORTANT! Do NOT use the 4 parameters, which is the handler, since it cancels out the help for sub-commands.
-   */
   .command(createContextCommandString, createContextDescription, createContextBuilder)
   .command(useContextCommandString, useContextDescription, useContextBuilder)
   .command(currentContextCommandString, currentContextDescription, __no_op__)
@@ -133,26 +100,17 @@ const yargsInstance = yargs(hideBin(process.argv))
       default: true,
     },
   })
-  .showHelpOnFail(false, 'Specify --help for available options') // default value is true.
-  .strict() // <--- any unknown command, or unknown flag, will raise an error.
+  .showHelpOnFail(false, 'Specify --help for available options')
+  .strict()
   .updateStrings({
-    'Positionals:': `${COLORS.blue}Positionals:${COLORS.stop}`, // <--- I will never use these. only Options, which I alias as 'Flags'.
+    'Positionals:': `${COLORS.blue}Positionals:${COLORS.stop}`,
     'Commands:': `${COLORS.blue}Commands:${COLORS.stop}`,
     'Options:': `${COLORS.blue}Flags:${COLORS.stop}`,
     'Examples:': `${COLORS.blue}Examples:${COLORS.stop}`,
   })
-  /**
-   * Configure an (e.g. --help) and implicit command that displays the usage string and exits the process. By default yargs enables help on the --help option.
-   *
-   * If present, the description parameter customizes the description of the help option in the usage string.
-   *
-   * If the boolean argument false is provided, it will disable --help.
-   *
-   * Note that any multi-char aliases (e.g. help) used for the help option will also be used for the implicit command. If there are no multi-char aliases (e.g. h), then all single-char aliases will be used for the command.
-   *
-   * If invoked without parameters, .help() will use --help as the option and help as the implicit command to trigger help output.
-   */
-  .help(false); // <--- help('help') & help() result in the same behavior.
+  .help(false);
+
+type ArgsV = { $0: any; _: Array<string> } & Record<string, string | number | boolean>;
 
 async function run() {
   const argv = yargsInstance.parse();
