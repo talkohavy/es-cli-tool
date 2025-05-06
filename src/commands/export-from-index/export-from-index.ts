f9efb25202c32fa55f340f2cd50677ec7e110bbd
Tal Kohavy <talkohavy@gmail.com>
2025-05-06 10:28:33 +0300

    renamed executeImportToIndexQuery to executeExportToFileQuery

--- src/commands/export-from-index/export-from-index.ts
+++ src/commands/export-from-index/export-from-index.ts
@@ -3,7 +3,7 @@ import { COLORS } from '../../common/constants/colors.js';
 import { getAllIndexesNames } from '../../common/utils/getAllIndexesNames.js';
 import { inquireSelectFromList } from '../../common/utils/inquires/inquireSelectFromList.js';
 import { logger } from '../../common/utils/logger/logger.js';
-import { executeExportToFileQuery } from './helpers/executeImportToIndexQuery.js';
+import { executeExportToFileQuery } from './helpers/executeExportToFileQuery.js';
 
 const ALL_INDEXES = 'all';
 
